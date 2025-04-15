1f1d322048cd09df968054b4ec873b789d99ff5e
perpouh <perpouh@gmail.com>
2025-04-15 21:56:50 +0900

    feat: PR時テスト
    brakemanは一旦保留

--- backend/app/mailers/application_mailer.rb
+++ backend/app/mailers/application_mailer.rb
@@ -1,4 +1,9 @@
+# frozen_string_literal: true
+
+# ApplicationMailer serves as the base class for all mailers in the application.
+# It provides default configurations for sending emails, such as the default sender email address
+# and the layout to be used for email templates.
 class ApplicationMailer < ActionMailer::Base
-  default from: "from@example.com"
-  layout "mailer"
+  default from: 'from@example.com'
+  layout 'mailer'
 end
--- backend/app/models/application_record.rb
+++ backend/app/models/application_record.rb
@@ -1,3 +1,8 @@
+# frozen_string_literal: true
+
+# This file is copied to spec/ when you run 'rails generate rspec:install'
+# and is used by all tests in the application.
+# This file defines the ApplicationRecord class, which serves as the base class for all models in the application.
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
 end
